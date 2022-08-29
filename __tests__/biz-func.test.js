050f037f5828e1e1d139d6a08ee4bf3e994e4e11
Kyle Chinick <kylechinick@icloud.com>
2022-08-29 15:05:55 -0700

    Initial commit
    Co-authored-by: Tyler Emmerson <tyleremmerson0216@gmail.com>
    Co-authored-by: Peter Vang <vang_peter06@yahoo.com>

---
+++ __tests__/biz-func.test.js
@@ -0,0 +1,88 @@
+// import GalacticDatabase from '../src/js/biz-func.js';
+
+// describe('GalacticDatabase', () => {
+//   test('Create an instance of the GalacticDatabase class', () => {
+//     const sectorOneDataBase = new GalacticDatabase();
+//     expect(sectorOneDataBase).toEqual({});
+//   });
+
+//   test('Create an instance of GalacticDatabase with a defined age property', () => {
+//     const sectorOneDataBase = new GalacticDatabase(20);
+//     expect(sectorOneDataBase.databaseUserAge).toEqual(20);
+//   });
+
+//   test('Return a number that is .24 times the value of databaseUserAge via the GalacticDatabase method mercuryYearsConverter()', () => {
+//     const sectorOneDataBase = new GalacticDatabase(20);
+//     expect(sectorOneDataBase.mercuryYearsConverter(20)).toEqual(4.8);
+//   });
+
+//   test('Return a number that is .62 times the value of databaseUserAge via the GalacticDatabase method venusYearsConverter()', () => {
+//     const sectorOneDataBase = new GalacticDatabase(20);
+//     expect(sectorOneDataBase.venusYearsConverter(20)).toEqual(12.4);
+//   });
+
+//   test('Return a number that is 1.88 times the value of databaseUserAge via the GalacticDatabase method marsYearsConverter()', () => {
+//     const sectorOneDataBase = new GalacticDatabase(20);
+//     expect(
+//       sectorOneDataBase.marsYearsConverter(sectorOneDataBase.databaseUserAge)
+//     ).toEqual(37.6);
+//   });
+
+//   test('Return a number that is 11.86 times the value of databaseUserAge via the GalacticDatabase method jupiterYearsConverter()', () => {
+//     const sectorOneDataBase = new GalacticDatabase(20);
+//     expect(sectorOneDataBase.jupiterYearsConverter(20)).toEqual(237.2);
+//   });
+
+//   test('Return result of subtracting the planet-converted user age from the planet-converted average lifespan via the GalacticDatabase method yearsRemainingCalculator()', () => {
+//     const sectorOneDataBase = new GalacticDatabase(20);
+//     expect(sectorOneDataBase.yearsRemainingCalculator(20, 100, 'mars')).toEqual(
+//       150.4
+//     );
+//   });
+
+//   // test('Return the number result of subtracting databaseUserAge from 100 via the GalacticDatabase method yearsRemainingCalculator() as a positive number if databaseUserAge is greater than 100 ', () => {
+//   //   const sectorOneDataBase = new GalacticDatabase(120);
+//   //   expect(sectorOneDataBase.yearsRemainingCalculator()).toEqual(20);
+//   // });
+
+//   test('Return a string stating the positive number of years above 100 that the user has lived based on databaseUserAge', () => {
+//     const sectorOneDataBase = new GalacticDatabase(120);
+//     expect(
+//       sectorOneDataBase.yearsRemainingCalculator(120, 100, 'mars')
+//     ).toEqual(
+//       'You have lived 37.6 years past average life expectancy, keep it up!'
+//     );
+//   });
+
+//   test('Return the converted average lifespan for mercury', () => {
+//     const sectorOneDataBase = new GalacticDatabase(20);
+//     expect(sectorOneDataBase.averageLifespanConverter(100, 'mercury')).toEqual(
+//       24
+//     );
+//   });
+//   test('Return the converted average lifespan for venus', () => {
+//     const sectorOneDataBase = new GalacticDatabase(20);
+//     expect(sectorOneDataBase.averageLifespanConverter(100, 'venus')).toEqual(
+//       62
+//     );
+//   });
+//   test('Return the converted average lifespan for mars', () => {
+//     const sectorOneDataBase = new GalacticDatabase(20);
+//     expect(sectorOneDataBase.averageLifespanConverter(100, 'mars')).toEqual(
+//       188
+//     );
+//   });
+//   test('Return the converted average lifespan for jupiter', () => {
+//     const sectorOneDataBase = new GalacticDatabase(20);
+//     expect(sectorOneDataBase.averageLifespanConverter(100, 'jupiter')).toEqual(
+//       1186
+//     );
+//   });
+
+//   test('Return error if no case is matched', () => {
+//     const sectorOneDataBase = new GalacticDatabase(20);
+//     expect(sectorOneDataBase.averageLifespanConverter(100, '')).toEqual(
+//       'Average lifespan conversion failed to execute properly'
+//     );
+//   });
+// });
---
+++ src/index.js
@@ -0,0 +1,42 @@
+// import $ from 'jquery';
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './css/styles.css';
+
+function amountTocoins(amount, coins) {
+  if (amount === 0) {
+    return [];
+  } else {
+    if (amount >= coins[0]) {
+      const remaining = amount - coins[0];
+      console.log(remaining);
+      return [coins[0]].concat(amountTocoins(remaining, coins));
+    } else {
+      console.log('coins: ' + coins);
+      coins.shift();
+
+      return amountTocoins(amount, coins);
+    }
+  }
+}
+console.log(amountTocoins(499, [25, 10, 5, 1]));
+
+// function counter(coinsArray) {
+//   const coinsTally = coinsArray.reduce(function (coinsFinal, coinsArray) {
+//     coinsFinal[coinsArray] = (coinsFinal[coinsArray] || 0) + 1;
+//     return coinsFinal;
+//   });
+// }
+
+// counter(amountTocoins(499, [25, 10, 5, 1]));
+
+// > toDos
+// ["hike", "go out to eat", "swim", "play games", "hike", "cook meals", "sleep", "swim", "play games", "hike", "swim", "play games"]
+
+// const toDoTally = toDos.reduce(function(voteTally, toDo) {
+//   voteTally[toDo] = (voteTally[toDo] || 0) + 1;
+//   return voteTally;
+// }, {});
+// toDos = coins
+// toDoTally = coinsTally
+// voteTally = coinsFinal
